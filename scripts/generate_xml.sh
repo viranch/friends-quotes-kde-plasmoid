43ef1532c944546c62252200fe143ae58b8653fc
Viranch Mehta <viranch.mehta@gmail.com>
2012-03-28 23:19:28 +0530

    Update (simplify) the XML scripts

--- scripts/generate_xml.sh
+++ scripts/generate_xml.sh
@@ -1,34 +1,7 @@
 #!/bin/sh
 
 path=$(echo $0 | sed "s/$(basename $0)//g")..
-python $path/scripts/parse.py $path/friends.html source.xml
-src=`pwd`
-mkdir /tmp/xml
-cd /tmp/xml
-cat << EOF > main.cpp
-#include <QDomDocument>
-#include <QFile>
-#include <iostream>
-
-int main(int argc, char* argv[])
-{
-    QFile file(argv[1]);
-    file.open(QIODevice::ReadOnly);
-    QDomDocument doc("friends-quotes");
-    doc.setContent(&file);
-    file.close();
-    std::cout << qPrintable(doc.toString());
-}
-EOF
-
-qmake -project
-echo "QT += xml" >> xml.pro
-qmake
-make
-cd $src
-/tmp/xml/xml source.xml > $path/contents/ui/friends.xml
-rm source.xml
-rm -rf /tmp/xml
+python $path/scripts/parse.py $path/friends.html $path/contents/ui/friends.xml
 echo
 echo ":: friends.xml generated and placed in contents/ui/friends.xml"
 echo
--- scripts/parse.py
+++ scripts/parse.py
@@ -5,7 +5,7 @@ s=f.read().replace('\n','').replace('\r','')
 f.close()
 
 f=open(sys.argv[2],'w')
-f.write('<?xml version="1.0" encoding="utf-8"?><friends-quotes>')
+f.write("<?xml version='1.0' encoding='utf-8'?>\n<friends-quotes>\n")
 
 def make_element(tag_name, tag_data):
     return '<'+tag_name+'><![CDATA['+tag_data+']]></'+tag_name+'>'
@@ -15,7 +15,7 @@ for ep in s.split('<h3>'):
     t = ep.split('</h3>')
     title = '<h3>'+t[0]+'</h3>'
     for q in t[1].split('<hr width="50%" />'):
-        f.write('<quote>'+make_element('title', title)+make_element('content',q)+'</quote>')
+        f.write(' <quote>\n  '+make_element('title', title)+'\n  '+make_element('content',q)+'\n </quote>\n')
 
-f.write('</friends-quotes>')
+f.write('</friends-quotes>\n')
 f.close()

bce6cd3e6d0dd435daf2b3a113a98a72bd33c63b
Viranch Mehta <viranch.mehta@gmail.com>
2012-11-09 19:55:59 +0530

    Fix XML generator script to not put empty quotes

--- scripts/parse.py
+++ scripts/parse.py
@@ -15,7 +15,7 @@ for ep in s.split('<h3>'):
     t = ep.split('</h3>')
     title = '<h3>'+t[0]+'</h3>'
     for q in t[1].split('<hr width="50%" />'):
-        f.write(' <quote>\n  '+make_element('title', title)+'\n  '+make_element('content',q)+'\n </quote>\n')
+        if q!="": f.write(' <quote>\n  '+make_element('title', title)+'\n  '+make_element('content',q)+'\n </quote>\n')
 
 f.write('</friends-quotes>\n')
 f.close()
